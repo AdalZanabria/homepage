0f3c5248b1b58ad7d6bac588f785c429d2946801
Adal Zanabria <57764806+AdalZanabria@users.noreply.github.com>
2020-10-03 01:54:08 -0500

    Add files via upload

---
+++ common/CV.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Adal Zanabria</title>
+        <link rel="stylesheet" href="../css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/main.css" />
+        
+    </head>
+    <body>
+        <div class="container-fluid bg">
+            <!--Encabezado-->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1
+                            class="display-3 text-center text-white"
+                            style="margin-top: 5%"
+                        >
+                            Hola, mi nombre es
+                        </h1>
+                        <h1
+                            class="display-3 text-center font-weight-bold text-white"
+                        >
+                            Adalberto Zanabria Castro
+                        </h1>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col">
+                        <h2 class="text-center text-white">
+                            Próximo Ingeniero en Sistemas y Tecnologías de la
+                            Información
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
+            <!--Tarjeta-->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card shadow" style="margin: 5%">
+                            <div class="card-header">
+                                <h4>Curriculum Vitae</h4>
+                            </div>
+                            <div class="card-body">
+                            <h1 class="display-4 text-center">Sobre Mí</h1>
+                            <p class="lead text-break">Soy un programador proactivo, creativo, versátil y me gusta trabajar en equipo, me apasiona la tecnología y el desarrollo web.<br/>
+                            Actualmente estudio el último cuatrimestre de mi carrera universitaria.<br/>
+                            Anteriormente estudié la carrera de Diseño Gráfico, que si bien no la terminé porque descubrí que lo mío era el código, me sirvió como trasfondo para ser un programador más integral.
+                            </p>
+                            <br/>
+                            <hr/>
+                            <br/>
+                                <h1 class="display-4 text-center">Tecnologías que utilizo</h1>
+                             <!--Inicia Carusel-->
+                             <div id="carousel" class="carousel slide" data-ride="carousel">
+
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel" data-slide-to="1"></li>
+                                    <li data-target="#carousel" data-slide-to="2"></li>
+                                    <li data-target="#carousel" data-slide-to="3"></li>
+                                    <li data-target="#carousel" data-slide-to="4"></li>
+                                    <li data-target="#carousel" data-slide-to="5"></li>
+                                    <li data-target="#carousel" data-slide-to="6"></li>
+                                    <li data-target="#carousel" data-slide-to="7"></li>
+                                    <li data-target="#carousel" data-slide-to="8"></li>
+                                    <li data-target="#carousel" data-slide-to="9"></li>
+                                    <li data-target="#carousel" data-slide-to="10"></li>
+                                </ol>
+
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                    <img src="../img/HTML5.svg.png" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">HTML</h5>
+                                        <p class="text-dark lead">Lenguaje de marcado para la elaboración de páginas web.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/CSS3.svg.png" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">CSS</h5>
+                                        <p class="text-dark lead">Lenguaje de estilos utilizado para describir la presentación de documentos HTML.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/js.jpg" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">JavaScript</h5>
+                                        <p class="text-dark lead">Lenguaje de programación ligero, interpretado, o compilado conocido como un lenguaje de scripting para páginas web.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/bootstrap.png" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">Bootstrap</h5>
+                                        <p class="text-dark lead">Biblioteca multiplataforma o conjunto de herramientas de código abierto para diseño de sitios y aplicaciones web.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/php.svg.png" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">PHP</h5>
+                                        <p class="text-dark lead">Lenguaje de programación de uso general que se adapta especialmente al desarrollo web.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/mysql.svg" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">MySQL</h5>
+                                        <p class="text-dark lead">Sistema de gestión de bases de datos relacional.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/Linux.svg.png" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">GNU/Linux</h5>
+                                        <p class="text-dark lead">Sistema operativo de open source y una plataforma de infraestructura de TI.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/Apache.png" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">Apache</h5>
+                                        <p class="text-dark lead">Servidor web HTTP de código abierto.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/AdobeCC.svg" class="d-block carousel-i" alt="..." style="filter: invert(100%)">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">Paquetería de Adobe</h5>
+                                        <p class="text-dark lead">Softwares de diseño gráfico, edición de video y diseño web.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/google_cloud.png" class="d-block carousel-i" alt="..."}>
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">Google Cloud Platform</h5>
+                                        <p class="text-dark lead">Plataforma de cómputo en la nube que uso para crear y administrar máquinas virtuales y alojar sitios y aplicaciones web.</p>
+                                    </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                    <img src="../img/Microsoft_Azure.svg" class="d-block carousel-i" alt="...">
+                                    <div class="carousel-caption d-none d-md-block">
+                                        <h5 class="text-dark">Microsoft Azure</h5>
+                                        <p class="text-dark lead">Plataforma de cómputo en la nube que uso para crear y administrar máquinas virtuales y alojar sitios y aplicaciones web.</p>
+                                    </div>
+                                    </div>
+                                </div>
+                                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </div>
+                            <!--Termina Carusel-->
+                            <br/>
+                            <hr/>
+                            <br/>
+                            <h1 class="display-4 text-center">Educación</h1>
+                            <p class="font-weight-bold">Licenciatura en Diseño Gráfico</p>
+                            <p style="margin-left: 40px;">2010/2012 <br/>
+                            Universidad de Colima<br/>
+                            Carrera Trunca hasta el cuarto semestre.</p>
+                            <p class="font-weight-bold">Licenciatura en Ingeniería en Sistemas y Tecnologías de la Información</p>
+                            <p style="margin-left: 40px;">2017/2020 <br/>
+                            Universidad del Valle de Atemajac<br/>
+                            Cursando el último cuatrimestre de la carrera.</p>
+                            <br/>
+                            <hr/>
+                            <br/>
+                            <h1 class="display-4 text-center">Experiencia</h1>
+                            <p class="font-weight-bold">Diseñador Editorial</p>
+                            <p class="text-break" style="margin-left: 40px;">12-2012/06-2014<br/>
+                            Realizando el diseño de la revista “Ciudadano en Movimiento”
+                            y el periódico “El Ciudadano” en la sección Ciudad de México.</p>
+                            <p class="font-weight-bold">Oficinista</p>
+                            <p class="text-break" style="margin-left: 40px;">01-2014/07-2017<br/>
+                            Realizando tareas generales de oficina para las áreas de
+                            Personal y Radiodiagnóstico en el Hospital General de Zona #27 
+                            Tlatelolco, Ciudad de México.</p>
+                            <br/>
+                            <hr/>
+                            <br/>
+                            <h1 class="display-4 text-center">Idiomas</h1>
+                            <p class="font-weight-bold">Español</p>
+                            <div class="progress" style="height: 30px;">
+                            <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">Nativo</div>
+                            </div>
+                            <p class="font-weight-bold">Inglés</p>
+                            <div class="progress" style="height: 30px;">
+                            <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">90%</div>
+                            </div>
+                    
+
+                            </div><!--termina body de tarjeta-->
+                        </div><!--Termina Tarjeta-->
+                    </div>
+                </div><!--Row de la tareta-->
+            </div><!--Contenedor del cuerpo-->
+        </div><!--Container-fluid-->
+
+        <footer class=" bg-dark text-white text-center" style="padding:1.5vh">
+            <div class="container">
+                <p>Adalberto Zanabria Castro </br>
+                adal.zanabria@gmail.com</br>
+                Tel. 5522149788</br>
+                Puerto Vallarta, Jalisco. México.</br>
+                2020</p>
+            </div>
+        </footer>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="../js/bootstrap.min.js"></script>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Adal Zanabria</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/main.css" />
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid bg">
+            <!--Encabezado-->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1
+                            class="display-3 text-center text-white"
+                            style="margin-top: 5%"
+                        >
+                            Hola, mi nombre es
+                        </h1>
+                        <h1
+                            class="display-3 text-center font-weight-bold text-white"
+                        >
+                            Adalberto Zanabria Castro
+                        </h1>
+                    </div>
+                    <div class="w-100"></div>
+                    <div class="col">
+                        <h2 class="text-center text-white">
+                            Próximo Ingeniero en Sistemas y Tecnologías de la
+                            Información
+                        </h2>
+                    </div>
+                </div>
+            </div>
+
+            <!--Tarjeta-->
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <div class="card shadow" style="margin: 5%">
+                            <div class="card-header">
+                                <h4>¿Qué servicio te gustaría ver?</h4>
+                            </div>
+                            <div class="card-body">
+
+
+                                    <div class="card-deck">
+
+                                        <div class="card shadow-sm imf-fluid">
+                                        <div class="card-header card-i" style="background-image: url('img/CV.jpg')"></div>
+                                            <div class="card-body">
+                                                <h5 class="card-title">
+                                                    Curriculum
+                                                </h5>
+                                                <p class="card-text">
+                                                    Mira mi Cutrriculum Vitae.
+                                                </p>
+                                                </div>
+                                                <div class="card-footer d-flex justify-content-center">
+                                                    <a
+                                                        class="btn btn-primary"
+                                                        href="common/CV.php"
+                                                        role="button"
+                                                        >Ir a Curriculum</a
+                                                    >
+                                                </div>
+                                        </div>
+
+                                        <div class="card shadow-sm img-fluid">
+                                                <div class="card-header card-i" style="background-image: url('img/Analizador.jpg')"></div>
+                                                <div class="card-body">
+                                                    <h5 class="card-title">Analizador Léxico de Lenguaje</h5>
+                                                    <p class="card-text">
+                                                        Un analizador hecho en JavaScript que recibe una cadena de texto dentro de un alfabeto permitido y evalúa si la cadena es válida o no.
+                                                    </p>
+                                                </div>
+                                                <div class="card-footer d-flex justify-content-center">
+                                                    <a
+                                                        class="btn btn-primary"
+                                                        href="https://adalzanabria.github.io/Analizador-Lexico/"
+                                                        role="button"
+                                                        >Ir a Analizador Léxico</a
+                                                    >
+                                                </div>                        
+                                            </div>
+
+                                    </div>
+
+                                    
+                                        <div class="card-deck">
+
+                                        <div class="card shadow-sm ">
+                                        <div class="card-header card-i" style="background-image: url('img/vtiger.png')"></div>
+                                            <div class="card-body">
+                                                <h5 class="card-title">
+                                                    CRM vtiger
+                                                </h5>
+                                                <p class="card-text">
+                                                    Prueba del CRM vtiger.
+                                                </p>
+                                            </div>
+                                            <div class="card-footer d-flex justify-content-center">
+                                                <a
+                                                    class="btn btn-primary"
+                                                    href="#"
+                                                    role="button"
+                                                    >Ir a vtiger</a
+                                                >
+                                            </div>
+                                        </div>
+
+                                            <div class="card shadow-sm img-fluid">
+                                            <div class="card-header card-i" style="background-image: url('img/odoo.png')"></div>
+                                                <div class="card-body">
+                                                    <h5 class="card-title">CRM Odoo</h5>
+                                                    <p class="card-text">
+                                                        Prueba del CRM Odoo.
+                                                    </p>
+                                                </div>
+                                                <div class="card-footer d-flex justify-content-center">   
+                                                    <a
+                                                        class="btn btn-primary"
+                                                        href="#"
+                                                        role="button"
+                                                        >Ir a Odoo</a
+                                                    >
+                                                </div>   
+                                            </div>
+                                    
+                                            
+                                    
+                                        </div>
+                                    
+
+                            </div>
+                        </div><!--Termina Tarjeta-->
+                    </div>
+                </div><!--Row de la tareta-->
+            </div><!--Contenedor del cuerpo-->
+        </div><!--Container-fluid-->
+
+        <footer class=" bg-dark text-white text-center" style="padding:1.5vh">
+            <div class="container">
+                <p>Adalberto Zanabria Castro </br>
+                adal.zanabria@gmail.com</br>
+                Tel. 5522149788</br>
+                Puerto Vallarta, Jalisco. México.</br>
+                2020</p>
+            </div>
+        </footer>
+    </body>
+</html>
